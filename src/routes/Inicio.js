76205f665774836325d858fbbea9f9a634284c68
EduardoAyora <eduardoaayora64@gmail.com>
2021-12-02 12:38:39 -0500

    Realizar validacion del formulario, el api solo funciona con pregunta 1 igual a 0

--- src/routes/Inicio.js
+++ src/routes/Inicio.js
@@ -49,11 +49,8 @@ export default function Inicio() {
   async function onSubmitInitialForm(e) {
     e.preventDefault()
     dispatch(submit())
-    console.log(state)
     if (!state.isValid) return
-    console.log('paso')
     let firstValue = state.questions[0].value
-    console.log(firstValue)
     const responseData = await fetch(`${url}/grupo-preguntas`, {
       method: 'POST',
       headers: {
