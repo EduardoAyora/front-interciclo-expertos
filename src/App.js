a0bafc97ef515b5dbf8e737433497223c81c3ad6
EduardoAyora <eduardoaayora64@gmail.com>
2021-12-02 13:56:12 -0500

    Empezar a agregar navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 
 import Inicio from './routes/Inicio'
 import Hechos from './routes/Hechos'
+import Navbar from './components/layout/Navbar'
 
 export default function App() {
   return (
     <div style={{ backgroundColor: '#f3f4f6' }}>
+      <Navbar />
       <div className='contenedor py-8 min-h-screen'>
         <Routes>
           <Route path='/hechos' element={<Hechos />} />
