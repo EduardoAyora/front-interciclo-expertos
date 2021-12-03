8673923c33a4c37c1c608a79fd5f23faed41899f
EduardoAyora <eduardoaayora64@gmail.com>
2021-12-02 22:46:48 -0500

    Agregar página hechos

--- src/routes/Hechos.js
+++ src/routes/Hechos.js
@@ -1,10 +1,54 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+const url = 'http://127.0.0.1:5000/api'
 
 export default function Otro() {
+  const [facts, setFacts] = useState([])
+
+  useEffect(() => {
+    async function sendRequest() {
+      const factsData = await fetch(`${url}/hechos`)
+      const facts = await factsData.json()
+      setFacts(facts)
+    }
+    sendRequest()
+  }, [])
+
   return (
-    <div>
-      <Link to='/'>Ir a inicio</Link>
-      <div>Pagina Hechos</div>
+    <div className='flex flex-col'>
+      <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+        <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+          <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+            <table className='min-w-full divide-y divide-gray-200'>
+              <thead className='bg-gray-50'>
+                <tr>
+                  <th
+                    scope='col'
+                    className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                  >
+                    Hechos
+                  </th>
+                </tr>
+              </thead>
+              <tbody className='bg-white divide-y divide-gray-200'>
+                {facts.map((fact, index) => (
+                  <tr key={index}>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                      <div className='flex items-center'>
+                        <div className='ml-4'>
+                          <div className='text-sm font-medium text-gray-900'>
+                            {fact}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
